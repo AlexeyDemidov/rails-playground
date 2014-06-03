37b79fec265247cd5a2270838251aa0031b90fdf
Alex L. Demidov <alexeydemidov@gmail.com>
2014-06-03 10:50:19 +0400

    Fix rubocop warnings
    Mostly line length wrap to 80 chars. Add few blank comments.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Playground::Application.config.session_store :cookie_store, key: '_playground_session'
+Playground::Application.config.session_store :cookie_store,
+                                             key: '_playground_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
